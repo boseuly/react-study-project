510910f17aac26e8e458a142854e779ec3f07fa6
boseul <tool0923@naver.com>
2023-03-12 17:12:43 +0900

    컴포넌트에서 props 사용하기

--- src/App.js
+++ src/App.js
@@ -4,13 +4,59 @@ import ExpenseItem from './components/ExpenseItem';
 
 // 원래는 js에서 HTML 문법을 사용할 수 없지만 JSX 덕분에 사용이 가능하다. 
 function App() {
+  const expenses = [
+    {
+      id : 'e1', 
+      title: 'Toilet Paper', 
+      amount: 94.12, 
+      date: new Date(2022, 02, 12),
+    },
+    {
+      id : 'e2', 
+      title: 'New TV', 
+      amount: 799.14, 
+      date: new Date(2023, 01, 12),
+    },
+    {
+      id : 'e3', 
+      title: 'Car Insurance', 
+      amount: 294.55, 
+      date: new Date(2023, 02, 22),
+    },
+    {
+      id : 'e4', 
+      title: 'New Desk', 
+      amount: 400.12, 
+      date: new Date(2023, 03, 11),
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <ExpenseItem></ExpenseItem>
+          <ExpenseItem 
+          title={expenses[0].title} 
+          amount={expenses[0].amount} 
+          date={expenses[0].date}
+          ></ExpenseItem>
+          <ExpenseItem 
+          title={expenses[1].title} 
+          amount={expenses[1].amount} 
+          date={expenses[1].date}
+          ></ExpenseItem>
+          <ExpenseItem 
+          title={expenses[2].title} 
+          amount={expenses[2].amount} 
+          date={expenses[2].date}
+          ></ExpenseItem>
+          <ExpenseItem 
+          title={expenses[3].title} 
+          amount={expenses[3].amount} 
+          date={expenses[3].date}
+          ></ExpenseItem>
         </p>
       </header>
     </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,21 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
+function ExpenseItem(props){    // props 모든 매개변수를 담고 있는 객체이다. 
+  const month = props.date.toLocaleString('en-US', {month : 'long' });
+  const day = props.date.toLocaleString('en-US', {day  : '2-digit' });
+  const year = props.date.getFullYear();
+
     return (
       <div className="expense-item">
-        <div>March 28th 2023</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+
         <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.65</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
         </div>
       </div>  
     );
