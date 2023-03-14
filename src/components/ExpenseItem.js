50ac1a2997cd36b33e23b8583bbe2f92a9321fca
boseul <tool0923@naver.com>
2023-03-14 14:58:18 +0900

    Card 컴포넌트 생성 및 합성 실습

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
+
 
 function ExpenseItem(props){    // props 모든 매개변수를 담고 있는 객체이다. 
   
 
     return (
-      <div className="expense-item">
+      <Card className="expense-item">
         <ExpenseDate date={props.date}/>
-
         <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{props.title}</h2> 
                 <div className="expense-item__price">{props.amount}</div>
         </div>
-      </div>  
+      </Card>  
     );
 }
 
